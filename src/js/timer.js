b9133ae154af3599c48daad46343fe17f3a46b6d
renjithspace <renjithrajeevvk@gmail.com>
2017-10-11 09:00:44 +0530

    Default options extender create

--- src/js/timer.js
+++ src/js/timer.js
@@ -11,6 +11,20 @@
                 hour: 0,
             },
         };
-        
+
+        // Extend defaul options
+        if (arguments.length && typeof arguments[0] === 'object') {
+            setOptions(this.options, arguments[0]);
+        }
+
+        // Set options
+        function setOptions(options, args) {
+            for (let arg in args) {
+                if (options.hasOwnProperty(arg)) {
+                    options[arg] = args[arg];
+                }
+            }
+        }
+
     };
 }());
