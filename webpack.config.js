7a0a3a30b047b75f22872f2b1d67a9b8ba99a2b1
renjithspace <renjithrajeevvk@gmail.com>
2017-10-27 17:10:38 +0530

    Webpack bundle minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: './src/js/timer.js',
     output: {
         filename: 'timer.js',
         path: path.resolve(__dirname, 'dist/js')
-    }
+    },
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 };
